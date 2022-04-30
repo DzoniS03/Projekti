2cfabf500e1f8fb74fd0980909dc86873f8513ae
DzoniS <93125767+DzoniS03@users.noreply.github.com>
2022-04-30 20:29:21 +0200

    Add files via upload

---
+++ E-Dnevnik/Sifarnik.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace E_Dnevnik
+{
+    public partial class Sifarnik : Form
+    {
+        SqlDataAdapter Adapter;
+        DataTable tabela;
+
+        string ime_tabele;
+        public Sifarnik(string tabela)
+        {
+            ime_tabele = tabela;
+            InitializeComponent();
+        }
+
+        private void Sifarnik_Load(object sender, EventArgs e)
+        {
+            Adapter = new SqlDataAdapter("SELECT * FROM " + ime_tabele, Konekcija.Connect());
+            tabela = new DataTable();
+            Adapter.Fill(tabela);
+
+            dataGridView1.DataSource = tabela;
+            dataGridView1.Columns["id"].ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable menjano = tabela.GetChanges();
+            Adapter.UpdateCommand = new SqlCommandBuilder(Adapter).GetUpdateCommand();
+            
+            if(menjano != null)
+            {
+                Adapter.Update(menjano);
+                this.Close();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
